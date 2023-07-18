6feacc1d5a468e445580b0b7256ded3d82948b3b
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2023-07-18 15:56:20 +0200

    iniziata creazione query di inserimento

--- Inserting.sql
+++ Inserting.sql
@@ -1,17 +1,78 @@
 use Football360;
 
-select * from Allenatore;
-
-
-INSERT INTO Allenatore (CodiceFiscale, Nome, Cognome, DataDiNascita, LuogoDiNascita, Email, NumeroDiTelefono, Stipendio, OttenimentoPatentino)
+INSERT INTO Allenatore (CodiceFiscale, Nome, Cognome, DataDiNascita, Citt‡DiNascita, Email, NumeroDiTelefono, Stipendio, OttenimentoPatentino)
 VALUES
-    ('CF1', 'Marco', 'Rossi', '1990-05-15', 'Milano', 'marco.rossi@example.com', '3351234567', 2000.50, '2022-01-10'),
-    ('CF2', 'Laura', 'Bianchi', '1985-11-22', 'Roma', 'laura.bianchi@example.com', '3398765432', 1800.75, '2021-08-05'),
-    ('CF3', 'Giovanni', 'Verdi', '1982-07-30', 'Napoli', 'giovanni.verdi@example.com', '3286547890', 2100.25, '2023-03-20'),
-    ('CF4', 'Francesca', 'Ferrari', '1995-02-18', 'Torino', 'francesca.ferrari@example.com', '3479876543', 1950.00, '2020-12-12'),
-    ('CF5', 'Alessio', 'Romano', '1988-09-12', 'Palermo', 'alessio.romano@example.com', '3467890123', 2200.20, '2022-06-15'),
-    ('CF6', 'Chiara', 'Gallo', '1991-04-25', 'Firenze', 'chiara.gallo@example.com', '3335678901', 1850.90, '2021-11-18'),
-    ('CF7', 'Luca', 'Conti', '1980-12-05', 'Bologna', 'luca.conti@example.com', '3347890123', 2400.00, '2023-02-28'),
-    ('CF8', 'Valentina', 'Santoro', '1993-06-08', 'Genova', 'valentina.santoro@example.com', '3401234567', 1750.30, '2020-10-22'),
-    ('CF9', 'Fabio', 'Rizzo', '1987-03-29', 'Catania', 'fabio.rizzo@example.com', '3390123456', 2050.80, '2022-04-17'),
-    ('CF10', 'Elena', 'Lombardi', '1998-08-01', 'Verona', 'elena.lombardi@example.com', '3426789012', 1900.40, '2021-09-09');
+    ('BTBRTR74M22B577V', 'Marco', 'Rossi', '1990-05-15', 'Milano', 'marco.rossi@example.com', '3351234567', 2000.50, '2022-01-10'),
+    ('CKDPMH90E18M081D', 'Laura', 'Bianchi', '1985-11-22', 'Roma', 'laura.bianchi@example.com', '3398765432', 1800.75, '2021-08-05'),
+    ('PVTNTT33M60F744Y', 'Giovanni', 'Verdi', '1982-07-30', 'Napoli', 'giovanni.verdi@example.com', '3286547890', 2100.25, '2023-03-20'),
+    ('CKIZFK42A64G372E', 'Francesca', 'Ferrari', '1995-02-18', 'Torino', 'francesca.ferrari@example.com', '3479876543', 1950.00, '2020-12-12'),
+    ('NBZCRV31R16E029F', 'Alessio', 'Romano', '1988-09-12', 'Palermo', 'alessio.romano@example.com', '3467890123', 2200.20, '2022-06-15'),
+    ('JFVKGB34P25L042I', 'Chiara', 'Gallo', '1991-04-25', 'Firenze', 'chiara.gallo@example.com', '3335678901', 1850.90, '2021-11-18'),
+    ('GVDZCZ45T27D845C', 'Luca', 'Conti', '1980-12-05', 'Bologna', 'luca.conti@example.com', '3347890123', 2400.00, '2023-02-28'),
+    ('VFBCTS47P66A996E', 'Valentina', 'Santoro', '1993-06-08', 'Genova', 'valentina.santoro@example.com', '3401234567', 1750.30, '2020-10-22'),
+    ('QLFTZL63M71B406V', 'Fabio', 'Rizzo', '1987-03-29', 'Catania', 'fabio.rizzo@example.com', '3390123456', 2050.80, '2022-04-17'),
+    ('MJMZRD42E19I932E', 'Elena', 'Lombardi', '1998-08-01', 'Verona', 'elena.lombardi@example.com', '3426789012', 1900.40, '2021-09-09');
+
+-- Acquisto (
+
+-- Arbitro (
+
+-- Articolo (
+
+-- Biglietteria (
+
+-- Biglietto (
+
+-- Calciatore (
+
+-- CategoriaPosto (
+
+-- CentroMedico (
+
+-- CentroSportivo (
+
+-- ClasseArbitrale (
+
+-- Cliente (
+
+-- Composizione (
+
+-- Corrispondenza (
+
+-- Dipendente (
+
+-- GuidaTecnica (
+
+-- Iscrizione (
+
+-- Lega (
+
+-- Marcatori (
+
+-- Negozio (
+
+-- Offerta (
+
+-- Partita (
+
+-- Presidente (
+
+-- Rosa (
+
+-- Societ‡Calcistica (
+
+-- Sponsor (
+
+-- Sponsorizzazione (
+
+-- Stadio (
+
+-- Stagione (
+
+-- Statisca (
+
+-- TernaArbitrale (
+
+-- Validit‡ (
+
+select * from Allenatore;
